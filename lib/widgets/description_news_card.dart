3ee00201bf2cea9af59b4446acd76fe738147520
JuanFGQ <juanfelipegq@outlook.es>
2023-05-10 00:00:48 +0200

    master

--- lib/widgets/description_news_card.dart
+++ lib/widgets/description_news_card.dart
@@ -2,30 +2,33 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
+import '../models/news/articles_info.dart';
+
 class DescriptionNewsCard extends StatelessWidget {
-  const DescriptionNewsCard({super.key});
+  final Article news;
+  final int index;
+  const DescriptionNewsCard(
+      {super.key, required this.news, required this.index});
 
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Row(
       children: [
-        Container(
-          margin: EdgeInsets.all(10),
-          width: size.width * 0.35,
-          height: size.height * 0.18,
-          // decoration: BoxD,
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(15),
-            child: const Image(
-
-                // colorBlendMode: BlendMode.colorBurn,
-
-                // opacity: 2.0,
-                // filterQuality: FilterQuality.medium,
-                fit: BoxFit.cover,
-                image: NetworkImage(
-                    'https://www.triviantes.com/wp-content/uploads/2021/03/catedral-de-manizales.jpg')),
+        ClipRRect(
+          borderRadius: BorderRadius.circular(15),
+          child: Container(
+            margin: const EdgeInsets.all(10),
+            width: size.width * 0.35,
+            height: size.height * 0.18,
+            color: Colors.red,
+            // decoration: BoxD,
+            child:
+                (news.urlToImage != null && news.urlToImage!.startsWith('http'))
+                    ? FadeInImage(
+                        placeholder: AssetImage('assets/barra_colores.gif'),
+                        image: NetworkImage(news.urlToImage!))
+                    : const Image(image: AssetImage('assets/no-image.png')),
           ),
         ),
         Expanded(
@@ -37,19 +40,20 @@ class DescriptionNewsCard extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Text('Source news/Authors'),
-                Container(
-                    // width: size.width * 0.5,
-                    // margin: EdgeInsets.all(10),
-                    // color: Colors.red,
-                    child: Text(
-                  'US debt ceiling: A simple guide to the crisis.',
-                  style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      fontStyle: FontStyle.normal),
-                )),
-                Text('May 05.2023')
+                Text(news.author!),
+                SizedBox(height: 5),
+                Flexible(
+                  child: Text(
+                    news.title,
+                    style: const TextStyle(
+                        overflow: TextOverflow.visible,
+                        // fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        fontStyle: FontStyle.normal),
+                  ),
+                ),
+                const SizedBox(height: 5),
+                Text(news.publishedAt.toString())
               ],
             ),
           ),
